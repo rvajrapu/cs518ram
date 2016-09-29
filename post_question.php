05b6bfbf3da10c2430b057cd9bd8cb2109f3293b
rvajrapu <rvajr001@odu.edu>
2016-09-29 18:09:02 -0400

    Adding textedit code and postquestion page

--- post_question.php
+++ post_question.php
@@ -7,11 +7,46 @@
 
     <div class="container">
 
-    <?php var_dump($_SESSION); ?>  
+    <div class="bs-example" data-example-id="basic-forms"> 
+      <form class="form-horizontal" action="#">
 
+      <div class="form-group">
+        <label for="Title">Title</label>
+        <input type="text" class="form-control" placeholder="Enter Title" autofocus>
+      </div>
+      <div class="form-group">
+        <label for="Question">Enter Question</label>
+        <div class="summernote">
+           
+        </div>
+      
+      </div>
+      <div class="form-group">
+        <label for="Title">Tags</label>
+        <select class="form-control">
+          <option>Science</option>
+          <option>Mathematics</option>
+          <option>Politics</option>
+          <option>Social</option>
+          <option>Other</option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit Question</button>  
+      </form>  
+      </div>
     </div> <!-- /container -->
+    <script>
+    $(document).ready(function() {
+        $('.summernote').summernote({
+          height: 200,                 // set editor height
+        });
+    });
+  </script>
+  <!-- Custom styles for this page -->
+  <link href="css/postquestion.css" rel="stylesheet">
+  <link href="libs/summernote/summernote.css" rel="stylesheet">
+  <script src="libs/summernote/summernote.js"></script>
 
-    <!-- Custom styles for this page -->
-    <link href="css/signin.css" rel="stylesheet">
-
-
+<?php
+  require_once("footer.php");
+?>
