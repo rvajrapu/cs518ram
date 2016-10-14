ab5cb57f6028776f84610dbb2a9f4940c76526d9
rvajrapu <rvajr001@odu.edu>
2016-10-14 16:53:11 -0400

    adding back index

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+  include("./includes/session.php");
+  include("./htmlheader.php");
+?>
+    <div  id="errormsg" role="alert">
+       <?php echo message(); ?>
+    </div>
+    <div class="container">
+
+      <form class="form-signin" id="signin" method ="post" action="validate.php">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <div class="form-group">
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Email address" name ="username" autofocus />
+        <p class='help-block'></p>
+        </div>
+        <div class="form-group">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" />
+        <p class='help-block'></p>
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+    <!-- Custom styles for this page -->
+    <link href="css/signin.css" rel="stylesheet">
+    <script src="js/validations.js"></script>
+
+
+<?php
+  include("./footer.php");
+?>
