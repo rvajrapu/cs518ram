b6fd898fe4d93e2dfb4a382f361dbf04c66c597c
rvajrapu <rvajr001@odu.edu>
2016-10-15 20:24:33 -0400

    Best answer posted

--- includes/functions.php
+++ includes/functions.php
@@ -81,7 +81,7 @@
 		$query .= "(A_TEXT, Q_ID, U_ID, CREATION_DATE) ";
 		$query .= "VALUES ('$answer', '$qid', $uid, CURDATE()) ";
 		$result_id = mysqli_query($connection, $query);
-		error_log("Inside query\n" . $query , 3, "C:/xampp/apache/logs/error.log");
+		//error_log("Inside query\n" . $query , 3, "C:/xampp/apache/logs/error.log");
 		// confirm_query($result_id);
 		if($result_id) {
 			$_SESSION["message"] = "Answer Posted";
@@ -104,10 +104,52 @@
 		}
 	}
 
+	function verify_asker($u_id) {
+		//error_log("\nverify_asker" . $u_id . $_SESSION["uid"], 3, "C:/xampp/apache/logs/error.log");
+		if ($_SESSION["uid"] == $u_id) {
+			
+			return true;
+		}
+		else return false;
+	}
+	function best_answer($a_id,$ba_id,$u_id,$q_id) {
+
+		//error_log("\nbest_answer" . $ba_id . $u_id , 3, "C:/xampp/apache/logs/error.log");
+		if (verify_asker($u_id) && ($a_id == $ba_id)) {
+			return '<i class="fa fa-check-square-o fa-3x" style = "color: #1d9d74" aria-hidden="true"></i>';
+		}
+		if (verify_asker($u_id) && !($a_id == $ba_id)) {
+			$output ='<i class="fa fa-square-o fa-3x" style = "color: #1d9d74" aria-hidden="true" onclick="best_answer(this)"></i>';
+			$output .= '<input type="hidden" name="forid" id="a_id" value = '.$a_id.' />';
+			$output .= '<input type="hidden" name="forid" id="u_id" value = '.$u_id.' />';
+			$output .= '<input type="hidden" name="forid" id="q_id" value = '.$q_id.' />';
+			return $output;
+		}
+
+	}
+	function update_answer($a_id,$q_id,$uid) {
+		global $connection;
+		
+		$query  = "UPDATE PTL_QUESTIONS SET ";
+		$query .= "BA_ID = '$a_id' ";
+		$query .= "WHERE Q_ID = '$q_id' AND U_ID = '$uid' ";
+		$result_id = mysqli_query($connection, $query);
+		error_log("Inside update query\n" . $query , 3, "C:/xampp/apache/logs/error.log");
+		// confirm_query($result_id);
+		if($result_id) {
+			$_SESSION["message"] = "Question Posted";
+			return true;
+
+		} else {
+			$_SESSION["message"] = "Database Error";
+			return false;
+		}
+	}
+
 	function get_result_1($ques_id) {
 		                                 	global $connection;   
                                             $query_1  = "
-                                                        SELECT Q_TITLE, Q_TEXT, Q_TAG, A_TEXT, PTL_ANSWERS.UP_VOTE, PTL_ANSWERS.DOWN_VOTE, BA_FLAG,PTL_USERS.FIRST_NAME,PTL_ANSWERS.CREATION_DATE 
+                                                        SELECT A_ID,Q_TITLE, Q_TEXT, Q_TAG, A_TEXT, PTL_ANSWERS.UP_VOTE, PTL_ANSWERS.DOWN_VOTE, BA_FLAG,PTL_USERS.FIRST_NAME,PTL_ANSWERS.CREATION_DATE 
                                                         FROM PTL_ANSWERS 
                                                         LEFT OUTER JOIN PTL_QUESTIONS ON PTL_ANSWERS.Q_ID = PTL_QUESTIONS.Q_ID 
                                                         LEFT OUTER JOIN PTL_USERS ON PTL_ANSWERS.U_ID = PTL_USERS.U_ID
@@ -124,7 +166,7 @@
 	function get_result_2($ques_id) {
 		global $connection;
                                             $query_2  = "
-														 SELECT  Q_TITLE, Q_TEXT, Q_TAG, PTL_QUESTIONS.CREATION_DATE,FIRST_NAME
+														 SELECT  Q_TITLE, Q_TEXT, Q_TAG, PTL_QUESTIONS.CREATION_DATE,FIRST_NAME,PTL_QUESTIONS.U_ID,BA_ID 
                                                          FROM PTL_QUESTIONS 
                                                          LEFT OUTER JOIN PTL_USERS ON PTL_QUESTIONS.U_ID =  PTL_USERS.U_ID
                                                          WHERE PTL_QUESTIONS.Q_ID = $ques_id
---
+++ post_bestanswer_ajax.php
@@ -0,0 +1,47 @@
+<?php
+  include("./includes/session.php");
+  include("./includes/db_connection.php");
+  include("./includes/functions.php");
+  include("./htmlheader.php");
+?>
+
+    <div class="container">
+
+    <?php $obj = $_POST['myData'];
+    
+    $a_id = $obj["a_id"];
+    $q_id = $obj["q_id"];
+    $uid = $_SESSION["uid"];
+    error_log("\nInside best answer" . $a_id , 3, "C:/xampp/apache/logs/error.log");
+
+    if (update_answer($a_id,$q_id,$uid)){
+
+        echo 'ok';
+
+    }
+    
+    
+    
+    
+    
+   
+
+    
+    ?>  
+
+    </div> <!-- /container -->
+
+    <!-- Custom styles for this page -->
+    <link href="css/signin.css" rel="stylesheet">
+   
+
+
+<?php
+// 4. Release returned data
+    mysqli_free_result($result);
+    require_once("footer.php");
+?> 
+
+    
+
+
